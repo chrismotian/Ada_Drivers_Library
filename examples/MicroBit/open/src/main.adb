078f8e29c93fb6be4148585e5f95089c824d2119
Piko <chrisjberlin@googlemail.com>
2020-01-26 16:19:37 +0800

    add winning condition achivable with dial
    Fixed position for the goal of the potentiometer is set up.
    The player need to rotate the potentiometer to this fixed position to turn off the light.
    Turned off light means that the player won.

--- examples/MicroBit/open/src/main.adb
+++ examples/MicroBit/open/src/main.adb
@@ -6,24 +6,39 @@ with HAL;      use HAL;
 -- real locking system could be a servo
 -- turned on light is same as a locked lock
 
--- the lock can't be unlocked because there is no input yet and therefore the light is turned on forever
 procedure Main is
 
    -- this is necessary to limit the idle;
-   countTime : Time_Ms := Clock;
+    countTime : Time_Ms := Clock;
    -- this is necessary to make the variable above comparable;
    countTimeUInt64 : UInt64;
    -- this is necessary to make the counter and current time/Clock unequal at start
    bootTime : constant UInt64 := 10000;
 
+   -- both are needed to have the current position and the goal for the potentiometer
+   dialValue : Analog_Value;
+   dialGoal : Analog_Value;
+
 begin
 
-   --  Turn on the LED connected to pin 0
+   -- Turn on the LED connected to pin 0
    Set (0, True);
 
+   dialValue := Analog(1);
+   -- Hardcoded "random" choice
+   dialGoal := 100;
+
    countTimeUInt64 := UInt64(countTime);
-   --  lockLight light up forever
-   while countTimeUInt64 < UInt64(Clock)+bootTime loop
+   --  lockLight is lighted up until the potentiometer is on the goal position
+   while countTimeUInt64 < UInt64(Clock)+bootTime and dialValue /= dialGoal loop
+
+      dialValue := Analog(1);
+
+   end loop;
+   --  game won
+   Set (0, False);
+   --  the program restarts if the program ends and therefore we include an empty infinite loop after we've won
+   loop
 
       null;
 
