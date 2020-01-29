e73a724378af5b8255e2f0c7a6cbd50e3886534d
Piko <chrisjberlin@googlemail.com>
2020-01-29 16:55:52 +0800

    add second player, logic for open safe game and servo.
    Alpha test can start

--- examples/MicroBit/open/src/main.adb
+++ examples/MicroBit/open/src/main.adb
@@ -3,87 +3,127 @@ with MicroBit.Time; use MicroBit.Time;
 with HAL;      use HAL;
 with NeoPixel; use NeoPixel;
 with LED_Demo; use LED_Demo;
+with MicroBit.Servos; use MicroBit.Servos;
 
 
--- goal is to program a light which will be used to substitute a real locking system
--- real locking system could be a servo
--- turned on light is same as a locked lock 
+-- goal is to program a real locking system or rather a game
 
 procedure Main is
 
    -- this is necessary to limit the idle;
-    countTime : Time_Ms := Clock;
+   countTime : Time_Ms := Clock;
    -- this is necessary to make the variable above comparable;
    countTimeUInt64 : UInt64;
    -- this is necessary to make the counter and current time/Clock unequal at start
-   bootTime : constant UInt64 := 10000;
-
-   -- both are needed to have the current position and the goal for the potentiometer
-   dialValue : Analog_Value;
-   dialGoal : Analog_Value;
+   bootTime : constant UInt64 := 5000;
 
    -- LED can be controlled with all colors
    Strip : LED_Strip := Create (Mode => GRB, Count => 18);
-   myColorValues : LED_Values := (LED_Green => 255,LED_Red => 255,LED_Blue=>255,LED_White=>255);
-   goalColorValues : LED_Values := (LED_Green => 255,LED_Red => 255,LED_Blue=>255,LED_White=>255);
 
+   dialValue : Analog_Value;
    dialValueColor : Analog_Value;
-   dialGoalColor : Analog_Value;
+   --to compare if Thief won or lost
+   Dial_Color_First : Integer;
+   Dial_Color_Second : Integer;
+   Dial_Color_Third : Integer;
+   Dial_Color_Fourth : Integer;
+
+   LED_Index : Natural := 4;
+
+   t : Analog_Value := 100;
 
 begin
+   -- close the Lock
+   Go(0,50);
+   Delay_Ms(1000);
+   Stop(0);
 
-   -- Turn on the LED connected to pin 0
-   Set (0, True);
-
-   -- Hardcoded "random" choice
-   dialGoal := 100;
-   -- The input needs to be adjusted because the conditional statement is made from red to red but I want red to purple
-   dialGoalColor := dialGoal*5/6;
-   
-   if dialGoalColor < 341 then
-      dialGoalColor := (dialGoalColor*3)/4;
-      goalColorValues := (LED_Green => UInt8(dialGoalColor),LED_Red => 255 - UInt8(dialGoalColor),LED_Blue=>0,LED_White=>100);
-   elsif dialGoalColor < 682 then
-      dialGoalColor := ((dialGoalColor-341)*3)/4;
-      goalColorValues := (LED_Blue => UInt8(dialGoalColor),LED_Green => 255 - UInt8(dialGoalColor),LED_Red=>0,LED_White=>100);
-   else
-      dialGoalColor := ((dialGoalColor-683)*3)/4;
-      goalColorValues := (LED_Red => UInt8(dialGoalColor),LED_Blue => 255 - UInt8(dialGoalColor),LED_Green=>0,LED_White=>100);
-   end if;
-
-   -- color can be changed for each LED individually
-   Set_Color (Strip, 0, myColorValues);
-   Set_Color (Strip, 7, goalColorValues);
-   -- change color command
-   Show (Strip, Kitronik_Write'Access);
-
-   countTimeUInt64 := UInt64(countTime);
-   --  lockLight is lighted up until the potentiometer is on the goal position.The Color on the Strip has to be changed at least one's
-   while countTimeUInt64 < UInt64(Clock)+bootTime loop
-
-      -- Reading the potentiometer
-      dialValue := Analog(1);
-      dialValueColor := dialValue*5/6;
-
-      if dialValueColor < 341 then
-         dialValueColor := (dialValueColor*3)/4;
-         myColorValues := (LED_Green => UInt8(dialValueColor),LED_Red => 255 - UInt8(dialValueColor),LED_Blue=>0,LED_White=>100);
-      elsif dialValueColor < 682 then
-         dialValueColor := ((dialValueColor-341)*3)/4;
-         myColorValues := (LED_Blue => UInt8(dialValueColor),LED_Green => 255 - UInt8(dialValueColor),LED_Red=>0,LED_White=>100);
+   for I in  1..4 loop
+      -- color can be changued for each LED individually
+      if I = 1 then
+         LED_Index := 4;
+      elsif I = 2 then
+         LED_Index := 3;
+      elsif I = 3 then
+         LED_Index := 7;
       else
-         dialValueColor := ((dialValueColor-683)*3)/4;
-         myColorValues := (LED_Red => UInt8(dialValueColor),LED_Blue => 255 - UInt8(dialValueColor),LED_Green=>0,LED_White=>100);
+         LED_Index := 0;
       end if;
 
-      Set_Color (Strip, 0, myColorValues);
-      Show (Strip, Kitronik_Write'Access);
+      -- signal for the players which light has to be controlled next
+      for I in 1..3 loop
+         Set_Color (Strip, LED_Index, All_Red_Color_Values);
+         Show (Strip, Kitronik_Write'Access);
+         Delay_Ms(500);
+         Set_Color (Strip, LED_Index, All_Green_Color_Values);
+         Show (Strip, Kitronik_Write'Access);
+         Delay_Ms(500);
+         Set_Color (Strip, LED_Index, All_Blue_Color_Values);
+         Show (Strip, Kitronik_Write'Access);
+         Delay_Ms(500);
+         Set_Color (Strip, LED_Index, All_White_Color_Values);
+         Show (Strip, Kitronik_Write'Access);
+         Delay_Ms(500);
+      end loop;
 
-      exit when dialValue = dialGoal;
+      countTime := Clock;
+      countTimeUInt64 := UInt64(countTime);
+   -- the player has 5 seconds to choose a color for a LED
+      while countTimeUInt64+bootTime > UInt64(Clock) loop
+
+      -- after reading the potentiometer...
+         dialValue := Analog(1);
+      -- the input needs to be adjusted because the coditional statement is made from red to red but I want red to purple
+         dialValueColor := dialValue*5/6;
+
+         if dialValueColor < 341 then
+            dialValueColor := (dialValueColor*3)/4;
+            Color_Values := (LED_Green => UInt8(dialValueColor),LED_Red => 255 - UInt8(dialValueColor),LED_Blue=>0,LED_White=>100);
+         elsif dialValueColor < 682 then
+            dialValueColor := ((dialValueColor-341)*3)/4;
+            Color_Values := (LED_Blue => UInt8(dialValueColor),LED_Green => 255 - UInt8(dialValueColor),LED_Red=>0,LED_White=>100);
+         else
+            dialValueColor := ((dialValueColor-683)*3)/4;
+            Color_Values := (LED_Red => UInt8(dialValueColor),LED_Blue => 255 - UInt8(dialValueColor),LED_Green=>0,LED_White=>100);
+         end if;
+
+         Set_Color (Strip, LED_Index, Color_Values);
+         Show (Strip, Kitronik_Write'Access);
+
+      end loop;
+
+      if I = 1 then
+         Dial_Color_First := Integer(dialValueColor);
+      elsif I = 2 then
+         Dial_Color_Second := Integer(dialValueColor);
+      elsif I = 3 then
+         Dial_Color_Third := Integer(dialValueColor);
+      else
+         Dial_Color_Fourth := Integer(dialValueColor);
+      end if;
+
    end loop;
-   --  game won
-   Set (0, False);
-   --  the program restarts if the program ends and therefore we include an empty infinite loop after we've won
+
+   --  Thief lose
+    if Dial_Color_First = Dial_Color_Second and Dial_Color_First = Dial_Color_Third and Dial_Color_First = Dial_Color_Fourth then
+
+         Go(0,50);
+     Delay_Ms(1000);
+      Stop(0);
+    --  Thief win
+   elsif (Dial_Color_First = Dial_Color_Second and Dial_Color_First = Dial_Color_Third) or (Dial_Color_First = Dial_Color_Second and Dial_Color_First = Dial_Color_Fourth) or (Dial_Color_Fourth = Dial_Color_Second and Dial_Color_Fourth = Dial_Color_Third) then
+
+   Go(0,0);
+     Delay_Ms(1000);
+      Stop(0);
+   --  Thief lose
+   else
+
+         Go(0,50);
+     Delay_Ms(1000);
+      Stop(0);
+   end if;
+   --  the program restarts if the program ends and therefore we include an empty infinite loop after the game is over
    loop
 
       null;
